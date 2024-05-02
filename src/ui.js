03fbd490432ec9d16312bad5a1ca42165b3b518b
pnuernberg <philipp.nuernberg@iconincar.com>
2024-05-02 09:48:45 +0200

    tests for multi file handling

---
+++ src/ui.js
@@ -0,0 +1,1 @@
+console.log('Im UI_TS')
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env, argv) => ({
 mode: argv.mode === 'production' ? 'production' : 'development',
@@ -7,8 +9,7 @@ mode: argv.mode === 'production' ? 'production' : 'development',
 // This is necessary because Figma's 'eval' works differently than normal eval
 devtool: argv.mode === 'production' ? false : 'inline-source-map',
   entry: {
-    app: './src/app.ts', // This is the entry point for our plugin code.
-    jszip: 'jszip'
+    app: ['./src/app.ts'] // This is the entry point for our plugin code.
   },
   module: {
     rules: [
@@ -28,4 +29,20 @@ devtool: argv.mode === 'production' ? false : 'inline-source-map',
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
+  plugins: [
+    new webpack.DefinePlugin({
+      global: {}, // Fix missing symbol error when running in developer VM
+    }),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './src/ui.html',
+      filename: 'ui.html',
+      chunks: ['ui'],
+    }),
+    new HtmlInlineScriptPlugin({
+      htmlMatchPattern: [/ui.html/],
+      scriptMatchPattern: [/.js$/],
+    }),
+  ]
 });
